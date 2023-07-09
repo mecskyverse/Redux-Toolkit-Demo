1614f6102c9f9e5959e9008a9c4e83731e4c81fa
Aakash <vermaaakash1000@gmail.com>
2023-07-09 12:31:45 +0530

    UserView added

---
+++ react-with-redux/src/features/user/UserView.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from './userSlice'
+export const UserView = () => {
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user)
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    return (
+        <div>
+            <h2>List Of Users: </h2>
+            {user.loading && <div>Loading....</div>}
+            {!user.loading && user.error ? < div > {error}</div> : null}
+
+            {
+                !user.loading && user.users.length ?
+                    (<ul>
+                        {user.users.map((user) => (
+                            <li key={user.id} style={{ color: 'white' }}>{user.name}</li>
+                        ))}
+                    </ul>)
+                    : null
+            }
+        </div >
+    )
+}
